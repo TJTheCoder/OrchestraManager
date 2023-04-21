cc3174adba126e812c724cddffe9b734b3c03a87
TJTheCoder <tanujt2k5@gmail.com>
2023-04-21 08:54:26 -0500

    Update KeyNode.java

--- TheOrchestraThingItself/src/main/java/com/mycompany/theorchestrathingitself/KeyNode.java
+++ TheOrchestraThingItself/src/main/java/com/mycompany/theorchestrathingitself/KeyNode.java
@@ -10,4 +10,13 @@ package com.mycompany.theorchestrathingitself;
  */
 public class KeyNode {
     
+    int index;      /*
+                    the pitches that are being modified
+                    (corresponds to the pitch variable) in the
+                    Note objects
+                    */
+    int modifier;   //how a specific pitch is changed
+    KeyNode left;   //lesser child for the KeyNode (based off index);]
+    KeyNode right;  //greater child for the KeyNode (based off index)
+    
 }
