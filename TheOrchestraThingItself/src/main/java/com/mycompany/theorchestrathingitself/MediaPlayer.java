37ccef5af506ae3524908da6f76ed16905ec36f0
austinbra <129190342+austinbra@users.noreply.github.com>
2023-04-24 00:32:36 -0500

    basic GUI

---
+++ TheOrchestraThingItself/src/main/java/com/mycompany/theorchestrathingitself/MediaPlayer.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.theorchestrathingitself;
+
+/**
+ *
+ * @author atxbr
+ */
+public class MediaPlayer {
+    
+    public Mediaplayer(){
+        public play(){
+            
+        }
+        public pause(){
+            
+        }
+        public skipFor(){
+            
+        }
+        public skipBack(){
+            
+        }
+        public next(){
+            
+        }
+        public back(){
+            
+        }
+    }
+}
